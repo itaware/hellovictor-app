e5001506f6089979503e21694bc15837c2b3d626
Sébastien Letélié <seb@itaware.eu>
2017-04-07 17:55:34 +0200

    add dialogs controls

--- src/app/quote/habitation/habitation.component.ts
+++ src/app/quote/habitation/habitation.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { QuoteService } from '../quote.service';
 import { Habitation } from '../habitation';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MdDialog, MdDialogRef } from '@angular/material';
+import { UncompleteDialogComponent } from '../uncomplete-dialog/uncomplete-dialog.component';
 
 @Component({
   selector: 'hv-habitation',
@@ -10,7 +12,12 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HabitationComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, public quoteService: QuoteService, private element: ElementRef) { 
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    public quoteService: QuoteService,
+    private element: ElementRef,
+    private dialog: MdDialog) {
   }
 
   ngOnInit() {
@@ -21,5 +28,17 @@ export class HabitationComponent implements OnInit {
     this.quoteService.calculAmount();
   }
 
+  valid() {
+    if (!this.quoteService.habitation.city && !this.quoteService.habitation.square) {
+      this.uncomplete();
+    } else {
+      this.router.navigate(['quote', 'habitation-add-on']);
+    }
+  }
+
+  uncomplete() {
+    const dialogContent: MdDialogRef<UncompleteDialogComponent> = this.dialog.open(UncompleteDialogComponent);
+    dialogContent.componentInstance.message = 'Pour valider il nous faut au moins votre <b>ville</b> et <b>quartier</b> d\'habitation SVP';
+  }
 
 }
