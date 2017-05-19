e003dc0fd0d33171f022a76302b5de7c20eec97e
Sébastien Letélié <seb@itaware.eu>
2017-05-19 14:46:28 +0200

    universal last config

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -6,21 +6,57 @@ const autoprefixer = require('autoprefixer');
 const postcssUrl = require('postcss-url');
 const cssnano = require('cssnano');
 
-const { NoEmitOnErrorsPlugin, LoaderOptionsPlugin, DefinePlugin, HashedModuleIdsPlugin } = require('webpack');
+const { NoEmitOnErrorsPlugin, DefinePlugin, HashedModuleIdsPlugin } = require('webpack');
 const { GlobCopyWebpackPlugin, BaseHrefWebpackPlugin, SuppressExtractedTextChunksWebpackPlugin } = require('@angular/cli/plugins/webpack');
 const { CommonsChunkPlugin, UglifyJsPlugin } = require('webpack').optimize;
 const { AotPlugin } = require('@ngtools/webpack');
 
 const nodeModules = path.join(process.cwd(), 'node_modules');
 const entryPoints = ["inline","polyfills","sw-register","scripts","styles","vendor","main"];
+const minimizeCss = true;
 const baseHref = "/";
 const deployUrl = "";
+const postcssPlugins = function () {
+        // safe settings based on: https://github.com/ben-eb/cssnano/issues/358#issuecomment-283696193
+        const importantCommentRe = /@preserve|@license|[@#]\s*source(?:Mapping)?URL|^!/i;
+        const minimizeOptions = {
+            autoprefixer: false,
+            safe: true,
+            mergeLonghand: false,
+            discardComments: { remove: (comment) => !importantCommentRe.test(comment) }
+        };
+        return [
+            postcssUrl({
+                url: (URL) => {
+                    // Only convert root relative URLs, which CSS-Loader won't process into require().
+                    if (!URL.startsWith('/') || URL.startsWith('//')) {
+                        return URL;
+                    }
+                    if (deployUrl.match(/:\/\//)) {
+                        // If deployUrl contains a scheme, ignore baseHref use deployUrl as is.
+                        return `${deployUrl.replace(/\/$/, '')}${URL}`;
+                    }
+                    else if (baseHref.match(/:\/\//)) {
+                        // If baseHref contains a scheme, include it as is.
+                        return baseHref.replace(/\/$/, '') +
+                            `/${deployUrl}/${URL}`.replace(/\/\/+/g, '/');
+                    }
+                    else {
+                        // Join together base-href, deploy-url and the original URL.
+                        // Also dedupe multiple slashes into single ones.
+                        return `/${baseHref}/${deployUrl}/${URL}`.replace(/\/\/+/g, '/');
+                    }
+                }
+            }),
+            autoprefixer(),
+        ].concat(minimizeCss ? [cssnano(minimizeOptions)] : []);
+    };
 
 
 
 
 module.exports = {
-  "devtool": "source-map",
+  "devtool": false,
   "resolve": {
     "extensions": [
       ".ts",
@@ -78,10 +114,22 @@ module.exports = {
           path.join(process.cwd(), "src/styles.scss")
         ],
         "test": /\.css$/,
-        "loaders": [
+        "use": [
           "exports-loader?module.exports.toString()",
-          "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-          "postcss-loader"
+          {
+            "loader": "css-loader",
+            "options": {
+              "sourceMap": false,
+              "importLoaders": 1
+            }
+          },
+          {
+            "loader": "postcss-loader",
+            "options": {
+              "ident": "postcss",
+              "plugins": postcssPlugins
+            }
+          }
         ]
       },
       {
@@ -89,11 +137,30 @@ module.exports = {
           path.join(process.cwd(), "src/styles.scss")
         ],
         "test": /\.scss$|\.sass$/,
-        "loaders": [
+        "use": [
           "exports-loader?module.exports.toString()",
-          "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-          "postcss-loader",
-          "sass-loader"
+          {
+            "loader": "css-loader",
+            "options": {
+              "sourceMap": false,
+              "importLoaders": 1
+            }
+          },
+          {
+            "loader": "postcss-loader",
+            "options": {
+              "ident": "postcss",
+              "plugins": postcssPlugins
+            }
+          },
+          {
+            "loader": "sass-loader",
+            "options": {
+              "sourceMap": false,
+              "precision": 8,
+              "includePaths": []
+            }
+          }
         ]
       },
       {
@@ -101,11 +168,28 @@ module.exports = {
           path.join(process.cwd(), "src/styles.scss")
         ],
         "test": /\.less$/,
-        "loaders": [
+        "use": [
           "exports-loader?module.exports.toString()",
-          "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-          "postcss-loader",
-          "less-loader"
+          {
+            "loader": "css-loader",
+            "options": {
+              "sourceMap": false,
+              "importLoaders": 1
+            }
+          },
+          {
+            "loader": "postcss-loader",
+            "options": {
+              "ident": "postcss",
+              "plugins": postcssPlugins
+            }
+          },
+          {
+            "loader": "less-loader",
+            "options": {
+              "sourceMap": false
+            }
+          }
         ]
       },
       {
@@ -113,11 +197,29 @@ module.exports = {
           path.join(process.cwd(), "src/styles.scss")
         ],
         "test": /\.styl$/,
-        "loaders": [
+        "use": [
           "exports-loader?module.exports.toString()",
-          "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-          "postcss-loader",
-          "stylus-loader?{\"sourceMap\":false,\"paths\":[]}"
+          {
+            "loader": "css-loader",
+            "options": {
+              "sourceMap": false,
+              "importLoaders": 1
+            }
+          },
+          {
+            "loader": "postcss-loader",
+            "options": {
+              "ident": "postcss",
+              "plugins": postcssPlugins
+            }
+          },
+          {
+            "loader": "stylus-loader",
+            "options": {
+              "sourceMap": false,
+              "paths": []
+            }
+          }
         ]
       },
       {
@@ -127,8 +229,20 @@ module.exports = {
         "test": /\.css$/,
         "loaders": ExtractTextPlugin.extract({
   "use": [
-    "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-    "postcss-loader"
+    {
+      "loader": "css-loader",
+      "options": {
+        "sourceMap": false,
+        "importLoaders": 1
+      }
+    },
+    {
+      "loader": "postcss-loader",
+      "options": {
+        "ident": "postcss",
+        "plugins": postcssPlugins
+      }
+    }
   ],
   "fallback": "style-loader",
   "publicPath": ""
@@ -141,9 +255,28 @@ module.exports = {
         "test": /\.scss$|\.sass$/,
         "loaders": ExtractTextPlugin.extract({
   "use": [
-    "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-    "postcss-loader",
-    "sass-loader"
+    {
+      "loader": "css-loader",
+      "options": {
+        "sourceMap": false,
+        "importLoaders": 1
+      }
+    },
+    {
+      "loader": "postcss-loader",
+      "options": {
+        "ident": "postcss",
+        "plugins": postcssPlugins
+      }
+    },
+    {
+      "loader": "sass-loader",
+      "options": {
+        "sourceMap": false,
+        "precision": 8,
+        "includePaths": []
+      }
+    }
   ],
   "fallback": "style-loader",
   "publicPath": ""
@@ -156,9 +289,26 @@ module.exports = {
         "test": /\.less$/,
         "loaders": ExtractTextPlugin.extract({
   "use": [
-    "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-    "postcss-loader",
-    "less-loader"
+    {
+      "loader": "css-loader",
+      "options": {
+        "sourceMap": false,
+        "importLoaders": 1
+      }
+    },
+    {
+      "loader": "postcss-loader",
+      "options": {
+        "ident": "postcss",
+        "plugins": postcssPlugins
+      }
+    },
+    {
+      "loader": "less-loader",
+      "options": {
+        "sourceMap": false
+      }
+    }
   ],
   "fallback": "style-loader",
   "publicPath": ""
@@ -171,9 +321,27 @@ module.exports = {
         "test": /\.styl$/,
         "loaders": ExtractTextPlugin.extract({
   "use": [
-    "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-    "postcss-loader",
-    "stylus-loader?{\"sourceMap\":false,\"paths\":[]}"
+    {
+      "loader": "css-loader",
+      "options": {
+        "sourceMap": false,
+        "importLoaders": 1
+      }
+    },
+    {
+      "loader": "postcss-loader",
+      "options": {
+        "ident": "postcss",
+        "plugins": postcssPlugins
+      }
+    },
+    {
+      "loader": "stylus-loader",
+      "options": {
+        "sourceMap": false,
+        "paths": []
+      }
+    }
   ],
   "fallback": "style-loader",
   "publicPath": ""
@@ -199,13 +367,57 @@ module.exports = {
       }
     }),
     new ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      "template": "./src/index.html",
+      "filename": "./index.html",
+      "hash": false,
+      "inject": true,
+      "compile": true,
+      "favicon": false,
+      "minify": false,
+      "cache": true,
+      "showErrors": true,
+      "chunks": "all",
+      "excludeChunks": [],
+      "title": "Webpack App",
+      "xhtml": true,
+      "chunksSortMode": function sort(left, right) {
+        let leftIndex = entryPoints.indexOf(left.names[0]);
+        let rightindex = entryPoints.indexOf(right.names[0]);
+        if (leftIndex > rightindex) {
+            return 1;
+        }
+        else if (leftIndex < rightindex) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
+    }),
     new BaseHrefWebpackPlugin({}),
     new ExtractTextPlugin({
       "filename": "[name].[contenthash:20].bundle.css",
       "disable": false
     }),
+    new SuppressExtractedTextChunksWebpackPlugin(),
     new DefinePlugin({
       "process.env.NODE_ENV": "\"production\""
+      }),
+    new HashedModuleIdsPlugin({
+      "hashFunction": "md5",
+      "hashDigest": "base64",
+      "hashDigestLength": 4
+    }),
+    new UglifyJsPlugin({
+      "mangle": {
+        "screw_ie8": true
+      },
+      "compress": {
+        "screw_ie8": true,
+        "warnings": false
+      },
+      "sourceMap": false
     }),
     new AotPlugin({
       "hostReplacementPaths": {
@@ -225,5 +437,8 @@ module.exports = {
     "module": false,
     "clearImmediate": false,
     "setImmediate": false
+  },
+  "devServer": {
+    "historyApiFallback": true
   }
 };
